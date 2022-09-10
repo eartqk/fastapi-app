f99c47b38a9424d6a74e3910d6b25f562fd225e5
Oleg Artamonov <eartqk@gmail.com>
2022-09-10 23:35:32 +0300

    Added path to router

--- src/workshop/api/__init__.py
+++ src/workshop/api/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+
+from .auth import router as auth_router
 from .operations import router as operations_router
 
 
 router = APIRouter()
+router.include_router(auth_router)
 router.include_router(operations_router)
